10205e3039ee57647e6caa4756230b811101b3dc
pmiglio21 <92951689+pmiglio21@users.noreply.github.com>
2025-09-25 21:43:22 -0400

    Removed BouncyBasketball scene and script finally

--- Entities/BasketballPlayers/BasketballPlayer.cs
+++ Entities/BasketballPlayers/BasketballPlayer.cs
@@ -551,7 +551,7 @@ namespace Entities
                     }
                 }
 
-                float ballSpeed = .8f;
+                float ballSpeed = .9f;
 
                 ParentBasketballCourtLevel.Basketball.LinearVelocity = new Vector3(basketballDestinationGlobalPosition.X - ParentBasketballCourtLevel.Basketball.GlobalPosition.X,
                                                                              //Mathf.Sin(Mathf.Pi / 4) * 20,
