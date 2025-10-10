5bc4fd4d24d336d7b1876bca133a269aea31480b
pmiglio21 <92951689+pmiglio21@users.noreply.github.com>
2025-10-10 13:45:37 -0400

    Added an EntityShadow that follows ball and players
    - Makes it easier to follow ball in the air when trying to rebound for example

---
+++ Entities/EntityShadow.cs
@@ -0,0 +1,27 @@
+﻿using Constants;
+using Enums;
+using Godot;
+using Levels;
+using System;
+using System.ComponentModel;
+
+namespace Entities
+{
+    public partial class EntityShadow: MeshInstance3D
+    {
+        [Export]
+        public Node3D ParentObject;
+
+        float groundY = -4.25f;
+
+        public override void _Ready()
+        {
+        }
+
+        public override void _Process(double delta)
+        {
+            GlobalPosition = new Vector3(ParentObject.GlobalPosition.X, groundY + 0.1f, ParentObject.GlobalPosition.Z);
+            GlobalRotation = Vector3.Zero;
+        }
+    }
+}
