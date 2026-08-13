f23464706f4b6c7084c5667d1fe8fa3d9e14e0c8
pmiglio21 <92951689+pmiglio21@users.noreply.github.com>
2026-08-13 07:54:20 -0400

    Added WebRtcClient and WebRtcServer
    - Going to commit this so I can switch back to the online-multiplayer-setup branch

---
+++ Online/WebRtcClient.cs
@@ -0,0 +1,25 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online
+{
+    public partial class WebRtcClient : Node
+    {
+        WebSocketMultiplayerPeer peer;
+
+        public override void _Ready()
+        {
+            peer = new();
+        }
+
+        private void ConnectToServer(string ipAddress)
+        {
+            peer.CreateClient("ws://127.0.0.1:8915");
+            GD.Print("Started client...");
+        }
+    }
+}
---
+++ Online/WebRtcServer.cs
@@ -0,0 +1,25 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online
+{
+    public partial class WebRtcServer : Node
+    {
+        WebSocketMultiplayerPeer peer;
+
+        public override void _Ready()
+        {
+            peer = new();
+        }
+
+        private void StartServer(string ipAddress)
+        {
+            peer.CreateServer(8915);
+            GD.Print("Started server...");
+        }
+    }
+}
